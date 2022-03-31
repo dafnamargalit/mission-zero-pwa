5ebe3025f4028d8ec1317aa58c4f91f4100d0f3b
Dafna <margalit.dafna77@gmail.com>
2022-03-31 16:24:49 -0600

    Revert "Revert "Merge pull request #2 from dafnamargalit/changeToClass""
    This reverts commit a50f8470c27e9c0eeb9b9467b130a6d8efb88ddb.

--- components/constants.js
+++ components/constants.js
@@ -2,7 +2,7 @@ import {
   Grid,
   HomeCommand, WorkCommand, 
   SolarCommand, SuperChargeCommand,
-  OutageCommand, Battery
+  OutageCommand, Battery, ChargeCar, ChargeHome
 } from '../icons';
 
 export const Descriptions = {
@@ -11,12 +11,15 @@ export const Descriptions = {
   outage: "Oh no the power is out! Whatever will we do? Super car to the rescue. In February and March 2021, sensors at the Mauna Loa observatory in Hawaii – which has tracked Earth’s atmospheric concentration of CO2 since the late 1950s – detected CO2 concentrations of more than 417 parts per million (ppm). Pre-industrial levels were 278 ppm, which means that humans are halfway to doubling the concentration of CO2 in the atmosphere compared to the period between 1750 and 1800.",
   grid: "Let's simulate what a modern grid looks like. Macro, micro, oh my! In February and March 2021, sensors at the Mauna Loa observatory in Hawaii – which has tracked Earth’s atmospheric concentration of CO2 since the late 1950s – detected CO2 concentrations of more than 417 parts per million (ppm). Pre-industrial levels were 278 ppm, which means that humans are halfway to doubling the concentration of CO2 in the atmosphere compared to the period between 1750 and 1800.",
   super: "Super charge that car RIGHT now. In February and March 2021, sensors at the Mauna Loa observatory in Hawaii – which has tracked Earth’s atmospheric concentration of CO2 since the late 1950s – detected CO2 concentrations of more than 417 parts per million (ppm). Pre-industrial levels were 278 ppm, which means that humans are halfway to doubling the concentration of CO2 in the atmosphere compared to the period between 1750 and 1800.",
-  battery: "Did you know this battery can recharge? Woah. In February and March 2021, sensors at the Mauna Loa observatory in Hawaii – which has tracked Earth’s atmospheric concentration of CO2 since the late 1950s – detected CO2 concentrations of more than 417 parts per million (ppm). Pre-industrial levels were 278 ppm, which means that humans are halfway to doubling the concentration of CO2 in the atmosphere compared to the period between 1750 and 1800."
+  battery: "Did you know this battery can recharge? Woah. In February and March 2021, sensors at the Mauna Loa observatory in Hawaii – which has tracked Earth’s atmospheric concentration of CO2 since the late 1950s – detected CO2 concentrations of more than 417 parts per million (ppm). Pre-industrial levels were 278 ppm, which means that humans are halfway to doubling the concentration of CO2 in the atmosphere compared to the period between 1750 and 1800.",
+  charging: "You can choose to either charge the car or charge the home! They can both power eachother!!!!!",
+  carCharge: "Charge the car! Did you know batteries can be rechargeable???",
+  homeCharge: "Charge the home! Did you know you can power the house with a car??? Woah."
 }
 
 export const CommandIcons = [
   { name: "grid", src: Grid },
-  { name: "home", src: HomeCommand },
+  { name: "home", src: HomeCommand, src2: ChargeCar, src3: ChargeHome },
   { name: "work", src: WorkCommand },
   { name: "solar", src: SolarCommand },
   { name: "super", src: SuperChargeCommand },
@@ -24,3 +27,5 @@ export const CommandIcons = [
   { name: "battery", src: Battery }
 
 ];
+
+export const CarColors = ['red', 'blue', 'green', 'purple'];
